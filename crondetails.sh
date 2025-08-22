2b3c9da9015ed3b65d525555707990ef30718d7a
dilyxs <selvan@adsayan.com>
2025-08-22 20:00:54 +0000

    crontab time changed to EST to UTC

--- crondetails.sh
+++ crondetails.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # === DeployAccountsMetaApi.sh ===
-# Runs at 4:48 PM and 5:18 PM Tuesday–Friday
-48 16,17 * * 2-5 /root/TFT_trading/DeployAccountsMetaApi.sh
-18 17 * * 2-5 /root/TFT_trading/DeployAccountsMetaApi.sh
+# Runs at 21:48 and 22:18 UTC Tuesday–Friday
+48 21 * * 2-5 /root/TFT_trading/DeployAccountsMetaApi.sh
+18 22 * * 2-5 /root/TFT_trading/DeployAccountsMetaApi.sh
 
-# Runs at 3:50 AM and 4:00 AM Monday only
-50 3 * * 1 /root/TFT_trading/DeployAccountsMetaApi.sh
-0 4 * * 1 /root/TFT_trading/DeployAccountsMetaApi.sh
+# Runs at 08:50 and 09:00 UTC Monday only
+50 8 * * 1 /root/TFT_trading/DeployAccountsMetaApi.sh
+0 9 * * 1 /root/TFT_trading/DeployAccountsMetaApi.sh
 
 # === TFT_limit_cleanup.sh ===
-# Runs at 4:51 PM Monday–Friday
-51 16 * * 1-5 /root/TFT_trading/TFT_limit_cleanup.sh
+# Runs at 21:51 UTC Monday–Friday
+51 21 * * 1-5 /root/TFT_trading/TFT_limit_cleanup.sh
 
 # === TFT_trade_close.sh ===
-# Runs at 4:51 PM Monday–Friday
-51 16 * * 1-5 /root/TFT_trading/TFT_trade_close.sh
+# Runs at 21:51 UTC Monday–Friday
+51 21 * * 1-5 /root/TFT_trading/TFT_trade_close.sh
 
 # === TFT_trading_infra.sh ===
-# Runs at 5:02 PM Tuesday–Friday
-2 17 * * 2-5 /root/TFT_trading/TFT_trading_infra.sh
+# Runs at 22:02 UTC Tuesday–Friday
+2 22 * * 2-5 /root/TFT_trading/TFT_trading_infra.sh
 
-# Runs at 3:52 AM Monday
-52 3 * * 1 /root/TFT_trading/TFT_trading_infra.sh
+# Runs at 08:52 UTC Monday
+52 8 * * 1 /root/TFT_trading/TFT_trading_infra.sh
 
